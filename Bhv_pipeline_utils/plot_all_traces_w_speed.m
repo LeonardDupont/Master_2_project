89085686cddcbe78444363965c412dcbc1f77ba0
Unknown <laineud@gmail.com>
2019-04-12 10:04:08 +0100

    Plot_all_traces w pseed

--- Bhv_pipeline_utils/plot_all_traces_w_speed.m
+++ Bhv_pipeline_utils/plot_all_traces_w_speed.m
@@ -1,55 +1,94 @@
 function [] = plot_all_traces_w_speed(cnintensity,xc,speed_labels,tm_speedMs)
 % Plots all calcium traces specified in cn_intensity on top of speed
 % landscape with borders specified in xc and labels specified in
-% speed_labels. 
+% speed_labels. April 2019. 
+% . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 
+%
+%    INPUT
+%
+%    cnintensity    the field 'intensity' with fluorescence traces 
+%                   xdim : fluorescence, ydim : cells
+%    xc             speedboundary limits (see get_speedregime_boundaries.m)
+%    speed_labels   T * 1 vector of labels for the speed trace
+%    tm_speedMs     speed trace, T * 1 vector of speed values (m.s-1)
+%                   'treadmill_speedMeansmoothed'
+%
+%    OUTPUT
+%
+%    plot with color relating to  speed regimes
+%
+% . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .
 
+ 
 
-calcium_data = cnintensity; 
-[x,y] = size(calcium_data);
+%% 1 - Checking if tm_speedMs and cnintensity times match (in seconds)
+% if huge discrepancy, then there is probably an issue and comparison is
+% useless. this has been a recurrent problem because of timestamp gestion.
+
+%Default
+FsTM = 1/3.3333e-04; %Hz
+FsIM = 30; %Hz
+tTM = length(speed_labels)/FsTM;
+tIM = length(cnintensity)/FsIM;
+epsilon = 5; %seconds
+
+if abs(tTM-tIM) > epsilon
+    warning(['Oops! Treadmill time (',num2str(tTM),'s) and imaging time (',num2str(tIM),...
+        's) do not seem to match... Di you use the right trial concatenation? Plotting nonetheless.'])
+end
+
+%% 2 - Plotting fluorescence traces
+
+timeIM = linspace(1,length(speed_labels),length(cnintensity));
+[x,y] = size(cnintensity);
 back_to_zero = zeros(x,y);
 for roi=1:y
-    back_to_zero(:,roi) = calcium_data(:,roi) - min(calcium_data(:,roi)); 
+    back_to_zero(:,roi) = cnintensity(:,roi) - min(cnintensity(:,roi)); 
 end
-time = linspace(0,length(speed_labels),x);
 
 
 
 figure, hold on
-title('Traces of all detected ROIs in the FOV')
+title('Speed v Fluorescence')
 offset = 0;
-positions = zeros(y,1); 
-labels = cell(y,1);
 
 for roi=1:y
-    plot(time,back_to_zero(:,roi) + offset,'color','k')
-    positions(roi,1) = offset;
+    plot(timeIM,back_to_zero(:,roi) + offset,'color','k')
     offset = offset + max(back_to_zero(:,roi)); 
-    labels(roi,1) = num2cell(roi);
 end
 
+%% 3 - Now plotting speed
+
 offset = offset + 1000; 
-tt = linspace(1,length(speed_labels),length(tm_speedMs));
-plot(tt,tm_speedMs*max(back_to_zero(:))*100 + offset,'color','k','LineWidth',1)
+position = offset;
+timeTM = linspace(1,length(speed_labels),length(tm_speedMs));
+plot(timeTM,tm_speedMs*max(back_to_zero(:))*100 + offset,'color','k','LineWidth',1)
 offset = offset + max(tm_speedMs*max(back_to_zero(:))*100);
+
+% This is just to label min speed, max speed and the speed trace
+position = [position, (position+offset)/2 ,offset];
+labels = {min(tm_speedMs),'speed (m.s^{-1})',max(tm_speedMs)};
+
 
 x0 = 10; 
 y0 = 10; 
 width = 5500; 
 height = 4000; 
 set(gcf,'position',[x0,y0,width,height])
-set(gca,'YTick',positions)
+set(gca,'YTick',position)
 set(gca,'TickLength',[0,0])
-%set(gca,'yticklabel',labels)
-set(gca,'yticklabel',{})
+set(gca,'yticklabel',labels)
 box off 
 axis tight
-xlabel('Datapoints (fs = 30Hz)')
-ylabel('ROIs')
+xlabel('Time (s)')
+ylabel('ROI fluorescence') %they occupy most of the space so OK 
 
-% ------------
+%% 4 - Speed-based color filling of time bits
+
 ncat = max(speed_labels);
 colormap = hsv(ncat+1);
 
+% . . . . . . . . . . This is just to have a nice legend  . . . . . . . . . 
 legend_spd = cell(ncat,1);
 for i = 1:ncat
     l(i) = plot([NaN,NaN], 'color', colormap(i,:));
@@ -58,6 +97,8 @@ for i = 1:ncat
 end
 legend(l, legend_spd,'AutoUpdate','off');
 
+
+%  . . . . . . . . . Now we fill areas based on boundaries  . . . . . . . .
 for k = 1:length(xc)-1
     xplus = xc(k+1);
     xminus = xc(k);
@@ -70,6 +111,19 @@ for k = 1:length(xc)-1
     a.FaceAlpha = 0.15;
     a.EdgeColor = 'none';
 end
+hold on
+xposi = linspace(1,length(speed_labels),10);
+xlab = cell(10,1);
+for k = 1:10
+    xlab(k) = num2cell(round(xposi(k)/FsTM));
+end
 
+%  . . . . . . .  This to get a time axis in seconds  . . . . . . . . . . .
+xticks(xposi)
+xticklabels(xlab)
 
 hold off 
+
+
+
+end
--- Ca_pipeline_utils/purkinje_artscape.m
+++ Ca_pipeline_utils/purkinje_artscape.m
@@ -22,6 +22,19 @@ for r = 1:N
    set(h, 'AlphaData', I) , hold on
 end
 
+if nargin == 2
+   others = linspace(1,length(masks),length(masks));
+   others(rois) = [];
+   for roth = 1:length(others)
+       art = others(roth);
+       I = masks{1,art};
+       c = [1 1 1];
+       full = cat(3,ones(S)*c(1),ones(S)*c(2),ones(S)*c(3));
+       h = imshow(full); hold on
+       set(h,'AlphaData',I*0.1)
+   end
+end
+
 hold off 
 
 end
